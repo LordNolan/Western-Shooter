7d3ebe86fbd1074e9271516775ebf42f11a4d37b
poemdexter <poemdexter@gmail.com>
2013-11-08 22:45:58 -0600

    Collisions!  created physics material to do so.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	
-	public float movementSpeed = 10;
-    public float turningSpeed = 60;
-    void Update() {
+	private float movementSpeed = 5;
+	
+    void FixedUpdate() {
         float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
         transform.Translate(horizontal, 0, 0);
         float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
         transform.Translate(0, -vertical, 0);
+		
+		rigidbody.velocity = Vector3.zero;
     }
 }
