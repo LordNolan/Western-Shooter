12f9d149bf55232687ed715f33de53e6f831da84
poemdexter <poemdexter@gmail.com>
2013-11-17 00:02:05 -0600

    Added Hitpoints to player and bandit.

--- Assets/Scripts/Bullet/BulletCollision.cs
+++ Assets/Scripts/Bullet/BulletCollision.cs
@@ -1,26 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletCollision : MonoBehaviour 
+public class BulletCollision : MonoBehaviour
 {
-	public ParticleSystem wallParticles;
+    public ParticleSystem wallParticles;
 	
-	void OnCollisionEnter(Collision collision)
-	{
-		// bullet hits wall
-		if (collision.gameObject.CompareTag("Wall"))
-		{
-			PlayParticleEffect(wallParticles);
-			Destroy(gameObject);
-		}
-	}
+    void OnCollisionEnter(Collision collision)
+    {
+        // bullet hits wall
+        if(collision.gameObject.CompareTag("Wall")) {
+            PlayParticleEffect(wallParticles);
+            Destroy(gameObject);
+        }
+        
+        // player bullet
+        if(CompareTag("PlayerBullet")) {
+            if(collision.gameObject.CompareTag("Enemy")) {
+                Debug.Log("we shot bandit");
+                collision.collider.SendMessage("TakeDamage", 1);
+                Destroy(gameObject);
+            }
+        }
+        
+        // enemy bullet
+        if(CompareTag("EnemyBullet")) {
+            if(collision.gameObject.CompareTag("Player")) {
+                Debug.Log("bandit shot us");
+                collision.collider.SendMessage("TakeDamage", 1);
+                Destroy(gameObject);
+            }
+        }
+    }
 	
-	void PlayParticleEffect(ParticleSystem particles)
-	{
-		// always be firing particles back as us
-		float y = transform.rotation.eulerAngles.y + 180;
+    void PlayParticleEffect(ParticleSystem particles)
+    {
+        // always be firing particles back as us
+        float y = transform.rotation.eulerAngles.y + 180;
 		
-		ParticleSystem part = GameObject.Instantiate(particles, transform.position, Quaternion.Euler(20,y,0)) as ParticleSystem;
+        ParticleSystem part = GameObject.Instantiate(particles, transform.position, Quaternion.Euler(20, y, 0)) as ParticleSystem;
         part.Play();
-	}
+    }
 }
