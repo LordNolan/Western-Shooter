dea9fe61633e0c4190578a6dcddb7aea36281bb3
poemdexter <poemdexter@gmail.com>
2013-11-27 19:16:51 -0600

    added Mido's sweet foot locker with collisions.  moved mobs folder to root of scripts folder.

---
+++ Assets/Scripts/Chest/OpenChest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenChest : MonoBehaviour
+{
+    void Start()
+    {
+        animation.PlayQueued("Idle_Closed", QueueMode.CompleteOthers);
+        animation.PlayQueued("Open", QueueMode.CompleteOthers);
+        animation.PlayQueued("Idle_Open", QueueMode.CompleteOthers);
+    }
+}
