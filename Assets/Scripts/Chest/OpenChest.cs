7f14e3a9cec7b519c00eb3d9cb9ddee414cd76ae
poemdexter <poemdexter@gmail.com>
2013-11-27 21:54:04 -0600

    Chest opens when you bump into it.

--- Assets/Scripts/Chest/OpenChest.cs
+++ Assets/Scripts/Chest/OpenChest.cs
@@ -3,10 +3,24 @@ using System.Collections;
 
 public class OpenChest : MonoBehaviour
 {
+    private bool isOpen = false;
+    
     void Start()
     {
         animation.PlayQueued("Idle_Closed", QueueMode.CompleteOthers);
+    }
+    
+    void PlayOpenAnimation()
+    {
         animation.PlayQueued("Open", QueueMode.CompleteOthers);
         animation.PlayQueued("Idle_Open", QueueMode.CompleteOthers);
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isOpen && collision.gameObject.CompareTag("Player")) {
+            PlayOpenAnimation();
+            isOpen = true;
+        }
     }
 }
