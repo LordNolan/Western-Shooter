e8b68d4659a8b04756d015d2e3bc00b515ffabd1
poemdexter <poemdexter@gmail.com>
2013-11-28 01:17:16 -0600

    added up/down for mouse movement.

--- Assets/Scripts/Game/MouseAimCamera.cs
+++ Assets/Scripts/Game/MouseAimCamera.cs
@@ -3,40 +3,20 @@ using System.Collections;
 
 public class MouseAimCamera : MonoBehaviour
 {
-    public GameObject target;
     public float rotateSpeed = 5;
-    public Vector3 cameraVerticalOffset;
-    Vector3 offset;
-    
-    public Vector3 defaultPosition;
-    public Quaternion defaultRotation;
-    
+    public bool invertedAxis = false;
     void LateUpdate()
     {
         if (GlobalParams.IsWorldGenComplete()) {
-            if (target != null && !GlobalParams.InNonPlayingState()) {
-                float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-                target.transform.Rotate(0, horizontal, 0);
-                float desiredAngle = target.transform.eulerAngles.y;
-                Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-                transform.position = target.transform.position - (rotation * offset);
-                transform.LookAt(target.transform);
-			
-                // added offset so player isn't middle of screen
-                transform.position += cameraVerticalOffset;
+            if (!GlobalParams.InNonPlayingState()) {
+                float vertical = Input.GetAxis("Mouse Y") * rotateSpeed * isInvertedAxis();
+                transform.Rotate(vertical, 0, 0);
             }
         }
     }
     
-    public void ResetCamera()
+    int isInvertedAxis()
     {
-        transform.position = defaultPosition;
-        transform.rotation = defaultRotation;
-    }
-	
-    void PlayerSpawned(GameObject player)
-    {
-        target = player;
-        offset = target.transform.position - transform.position;
+        return (invertedAxis) ? 1 : -1;
     }
 }
