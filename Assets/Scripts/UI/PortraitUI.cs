6f87142bfaa232f82138124d5f43c1cf54dae569
poemdexter <poemdexter@gmail.com>
2013-11-29 14:35:35 -0600

    shifty eyes cowgirl part of UI.

---
+++ Assets/Scripts/UI/PortraitUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortraitUI : MonoBehaviour
+{
+    public Texture2D p_forward;
+    public Texture2D p_left;
+    public Texture2D p_right;
+    
+    p_State currentState;
+    enum p_State
+    {
+        forward,
+        left,
+        right
+    }
+    
+    private GUITexture p_texture;
+    
+    void Awake()
+    {
+        p_texture = GetComponent<GUITexture>();
+        currentState = p_State.forward;
+    }
+    
+    private double timer = 0;
+    private double waitTime = 1;
+    void Update()
+    {
+        if ((timer += Time.deltaTime) > waitTime) {
+            SwapTexture();
+            timer = 0;
+        }
+    }
+    
+    void SwapTexture()
+    {
+        switch (currentState) {
+        case p_State.forward:
+            p_texture.texture = p_left;
+            currentState = p_State.left;
+            break;
+        case p_State.left:
+            p_texture.texture = p_right;
+            currentState = p_State.right;
+            break;
+        case p_State.right:
+            p_texture.texture = p_forward;
+            currentState = p_State.forward;
+            break;
+        }
+    }
+}
