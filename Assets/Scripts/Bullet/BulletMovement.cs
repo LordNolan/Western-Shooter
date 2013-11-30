75d144f86ccfb861032e972ee6f17b0219bad724
poemdexter <poemdexter@gmail.com>
2013-11-29 23:24:17 -0600

    bullet destroys itself after 3 seconds.

--- Assets/Scripts/Bullet/BulletMovement.cs
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class BulletMovement : MonoBehaviour
 {
     public float speed;
-	
+    public float selfDestructTime = 3.0f;
+    float currentTime = 0;
+    
     void FixedUpdate()
     {
         transform.position += transform.up * speed * Time.deltaTime;
+        if ((currentTime += Time.deltaTime) > selfDestructTime) {
+            Destroy(gameObject);
+        }
     }
 }
