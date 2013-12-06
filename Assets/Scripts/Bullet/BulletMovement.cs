63dc2ed58e6971288ec1b59372686fec9e43e634
poemdexter <poemdexter@gmail.com>
2013-12-06 01:30:33 -0600

    bullet moves via rigidbody.velocity instead of transform.position updates.

--- Assets/Scripts/Bullet/BulletMovement.cs
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class BulletMovement : MonoBehaviour
 {
-    public float speed;
+    
     public float selfDestructTime = 3.0f;
     float currentTime = 0;
     
+    [HideInInspector]
+    public float speed;
+    
     void FixedUpdate()
     {
-        transform.position += transform.up * speed * Time.deltaTime;
+        rigidbody.velocity = transform.up * speed * Time.deltaTime;
+        //transform.position += transform.up * speed * Time.deltaTime;
         if ((currentTime += Time.deltaTime) > selfDestructTime) {
             Destroy(gameObject);
         }
