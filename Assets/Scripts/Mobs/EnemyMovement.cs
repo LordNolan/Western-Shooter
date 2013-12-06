efac37d8af045caeed281e0c3aca12fdaac406d9
poemdexter <poemdexter@gmail.com>
2013-12-05 23:10:57 -0600

    fixed bug bandit moving through walls when it hit seams and flipped flag twice quickly.

--- Assets/Scripts/Mobs/EnemyMovement.cs
+++ Assets/Scripts/Mobs/EnemyMovement.cs
@@ -6,11 +6,17 @@ public class EnemyMovement : MonoBehaviour {
     public float speed;
     bool moveRight = true;
     
+    // adding delay so that if we hit seam between two walls, we don't flip flag twice and go through wall
+    public float collisionDelay = .5f;
+    float currentTime = 0;
+    
 	void Update () 
     {
         if (GlobalParams.IsMobAIDelayComplete() && !GlobalParams.InNonPlayingState() && !GetComponent<Hitpoints>().mobDead) {
             transform.Translate(Vector3.right * speed * GetDirection() * Time.deltaTime);
         }
+        
+        currentTime += Time.deltaTime;
 	}
     
     float GetDirection()
@@ -21,8 +27,20 @@ public class EnemyMovement : MonoBehaviour {
     void OnCollisionEnter(Collision collision) 
     {
         // mob hits wall
-        if (collision.gameObject.CompareTag("Wall")) {
+        if (collision.gameObject.CompareTag("Wall") && CanCollideWithWall()) {
             moveRight = !moveRight;
         }
+    }
+    
+    // checks if enough time has passed between collisions
+    bool CanCollideWithWall()
+    {
+        if (currentTime >= collisionDelay)
+        {
+            currentTime = 0;
+            return true;
+        }
+        else 
+            return false;
     }
 }
