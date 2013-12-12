d2dac985eeb11da7ec40dce66fb523cb3da5e89b
poemdexter <poemdexter@gmail.com>
2013-12-11 22:27:44 -0600

    ammo now set to 20 and decrements on fire.

--- Assets/Scripts/Mobs/PlayerFireWeapon.cs
+++ Assets/Scripts/Mobs/PlayerFireWeapon.cs
@@ -5,7 +5,7 @@ public class PlayerFireWeapon : MonoBehaviour
 {
     public float heightOffset = 0.3f;
     public float directionOffset = 0.3f;
-    
+    public int ammoAmount = 20;
     public float fireDelayTime;
     float currentTime;
     
@@ -22,8 +22,14 @@ public class PlayerFireWeapon : MonoBehaviour
         if (currentTime > fireDelayTime && !GlobalParams.InNonPlayingState() && Input.GetMouseButton(0)) { // left click or held down
             currentTime = 0; // reset delay            
             transform.FindChild("Pistol").GetComponent<RaycastFire>().Fire(GetBulletSpawnPosition(), GetForwardDirection()); // fire raycast
+              GameObject.FindWithTag("UI").BroadcastMessage("AmmoSpent", GetAmmoAmount());
             audio.Play();
         }
+    }
+    
+    int GetAmmoAmount()
+    {
+        return Mathf.Min(0, --ammoAmount);
     }
     
     Vector3 GetBulletSpawnPosition()
---
+++ Assets/Scripts/UI/AmmoUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoUI : MonoBehaviour 
+{
+    public void AmmoSpent(int newAmount)
+    {
+          guiText.text = ": " + newAmount;
+    }
+}
