6cccd62c06638f0210e7ad29a389e2abaec108ed
poemdexter <poemdexter@gmail.com>
2013-12-15 21:20:44 -0600

    AI work on cactus.  Has idle and aggressive state.  Idle waits for player to be in range.  Aggressive will fire shots.  Added sound and sprite animation for being alerted to player.

--- Assets/Scripts/Mobs/EnemyFireWeapon.cs
+++ Assets/Scripts/Mobs/EnemyFireWeapon.cs
@@ -5,44 +5,38 @@ public class EnemyFireWeapon : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public float bulletSpeed;
-    public double firingRange = 4.0;
     private double currentTime = 0;
     public float delay = 1.0f;
     public float delayVariance = 0.2f;
     private double currentDelay;
+    public AudioSource gunshotSound;
     
     void Start()
     {
         currentDelay = GetNextFiringTime();
     }
     
-    void Update()
+    public void FireAtPlayer()
     {
-        if (GlobalParams.IsMobAIDelayComplete() && !GetComponent<Hitpoints>().mobDead) {
-            GameObject player = (GameObject)GameObject.FindWithTag("Player");
-            
-            // fire at the player
-            if (player != null && !GlobalParams.InNonPlayingState() && WithinFiringRange(player.transform.position) && (currentTime += Time.deltaTime) >= currentDelay) {
-                currentTime = 0;
-                SetBulletSpeed();
-                // create bullet rotated to point at player (not camera)
-                float x = bulletPrefab.transform.localEulerAngles.x;
-                Vector3 relativePos = player.transform.position - transform.position;
-                Quaternion rotation = Quaternion.LookRotation(relativePos);
-                Instantiate(bulletPrefab, transform.position, Quaternion.Euler(x, rotation.eulerAngles.y, 0));
-                audio.Play();
-            }
+        GameObject player = (GameObject)GameObject.FindWithTag("Player");
+        
+        // fire at the player
+        if ((currentTime += Time.deltaTime) >= currentDelay) {
+            currentTime = 0;
+            SetBulletSpeed();
+            // create bullet rotated to point at player (not camera)
+            float x = bulletPrefab.transform.localEulerAngles.x;
+            Vector3 relativePos = player.transform.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            Instantiate(bulletPrefab, transform.position, Quaternion.Euler(x, rotation.eulerAngles.y, 0));
+            gunshotSound.Play();
         }
+        
     }
     
     double GetNextFiringTime()
     {
         return Random.Range(delay - delayVariance, delay + delayVariance);
-    }
-    
-    bool WithinFiringRange(Vector3 player)
-    {
-        return (Vector3.Distance(player, transform.position) < firingRange);
     }
     
     void SetBulletSpeed()
