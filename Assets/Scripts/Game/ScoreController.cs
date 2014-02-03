f4b0d28e5d6b66d39047c126f8ecec76ac981607
Daniel Fairley <poemdexter@gmail.com>
2014-02-02 18:48:40 -0800

    score multiplier in.  2 seconds to maintain kill streak, every 3 kills increases multiplier by 1.

--- Assets/Scripts/Game/ScoreController.cs
+++ Assets/Scripts/Game/ScoreController.cs
@@ -4,6 +4,13 @@ using System.Collections;
 public class ScoreController : MonoBehaviour
 {
     public int score { get; private set; }
+    public float multiplierTime;
+
+    int scoreMulti = 1;
+    int killCounter = 0;
+    float currentTime;
+    bool isMulti = false;
+
     ScoreUI ui;
 
     void Start()
@@ -13,12 +20,41 @@ public class ScoreController : MonoBehaviour
 
     public void AddScore(int amount)
     {
-        score += amount;
+        score += scoreMulti * amount;
         ui.UpdateScoreboard(score);
+        isMulti = true;
+        currentTime = 0;
+        killCounter++;
+        RecalculateMulti();
+    }
+
+    void RecalculateMulti()
+    {
+        scoreMulti = Mathf.FloorToInt(killCounter / 3) + 1;
+        Debug.Log(scoreMulti);
     }
 
     public void ResetScore()
     {
         score = 0;
+        scoreMulti = 1;
+    }
+
+    void Update()
+    {
+        if (isMulti && ((currentTime += Time.deltaTime) >= multiplierTime)) {
+            scoreMulti = 1;
+            currentTime = 0;
+            killCounter = 0;
+            isMulti = false;
+        }
     }
 }
+
+/*
+ * score multiplier
+ * on first kill, start one second timer.
+ * if timer expires, reset kill counter.
+ * if another kill occurs during timer, timer resets and kill count increments
+ * for every 3 kills in kill counter, multiplier increased by 1
+ */
